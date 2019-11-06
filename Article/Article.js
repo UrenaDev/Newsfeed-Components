d7c2a2000c33b31ca218f9f34bea9f35cececca3
Ben Urena <urenadev@gmail.com>
2019-11-06 01:19:09 -0600

    Created function and elements

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,21 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+
+const summonArticle = (title, date, p1, p2, p3) => {
+  //creating elements
+  const article = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const par1 = document.createElement('p');
+  const par2 = document.createElement('p');
+  const par3 = document.createElement('p');
+  const button = document.createElement('span');
+
+
+  //creating children
+
+  
+
+}
